9083205461cbb0cb3c7778c1a20354bbe10a6241
Adriano Tsurumaki <adrianotsurumaki@gmail.com>
2025-06-04 03:03:51 -0300

    feat: configure program.cs

---
+++ server/DependecyInjection.cs
@@ -0,0 +1,14 @@
+﻿using server.Infrastructure.Persistence.Repositories;
+using server.Repositories.Interfaces;
+
+namespace server;
+
+public static class DependecyInjection
+{
+    public static IServiceCollection AddDependencies(this IServiceCollection services)
+    {
+        services.AddScoped<ITransactionRepository, TransactionRepository>();
+
+        return services;
+    }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -1,11 +1,19 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using server;
+using server.Infrastructure.Persistence.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDependencies();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<FinanceDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -21,5 +29,15 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+Console.WriteLine("Starting database migration...");
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<FinanceDbContext>();
+    db.Database.Migrate();
+}
+
+Console.WriteLine("Database migration completed successfully.");
 
 app.Run();
